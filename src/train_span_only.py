8cc71dc370f6fd5c451ba55fe003afc764978297
Anemily Machina <anemily.machina@gmail.com>
2026-03-18 13:14:08 -0400

    playing with training parameters

--- src/train_span_only.py
+++ src/train_span_only.py
@@ -1,8 +1,10 @@
 from make_pytroch_dataset import make_dataset, span_only_collate
-from utils import load_ai_model4token_class
+from utils import load_ai_model4token_class, load_tokenizer
 
 import torch
 from torch.utils.data import DataLoader
+
+from transformers import Trainer, TrainingArguments
 
 if torch.cuda.is_available():
     print()
@@ -24,30 +26,28 @@ def main(ai_name):
 
     dataset = make_dataset(ai_name, split="train", max_size=8192, span_only=True)
 
-    dataloader = DataLoader(
-        dataset,
-        batch_size=2,
-        collate_fn=span_only_collate,
-        shuffle=False,
-        # num_workers=0,
-        # prefetch_factor=1,
+    training_args = TrainingArguments(
+        output_dir="./checkpoints/test",
+        learning_rate=2e-5,
+        per_device_train_batch_size=2,
+        per_device_eval_batch_size=2,
+        num_train_epochs=2,
+        weight_decay=0.01,
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        load_best_model_at_end=True,
+        push_to_hub=False,
     )
 
-    for batch in dataloader:
+    trainer = Trainer(
+        model=ai_model.to(device),
+        args=training_args,
+        train_dataset=dataset,
+        eval_dataset=dataset,
+        data_collator=span_only_collate,
+    )
 
-        batch = {k: v.to(device) for k, v in batch.items()}
-
-        # labels = batch.pop("labels")
-
-        output = ai_model(**batch)
-
-        print(output.logits.size())
-
-        print(output)
-
-        exit()
-
-    print(ai_model)
+    trainer.train()
 
 
 if __name__ == "__main__":
