04b32f4f2df7f740ec4368c58d392d70d2f4e2c1
Anemily Machina <anemily.machina@gmail.com>
2026-03-18 13:39:16 -0400

    playing with training parameters

--- src/train_span_only.py
+++ src/train_span_only.py
@@ -22,26 +22,33 @@ else:
 
 
 def compute_metrics(eval_pred):
-    metric1 = load_metric("precision")
-    metric2 = load_metric("recall")
-    metric3 = load_metric("f1")
+    # metric1 = load_metric("precision")
+    # metric2 = load_metric("recall")
+    # metric3 = load_metric("f1")
     metric4 = load_metric("accuracy")
 
     logits, labels = eval_pred
+
+    print(logits.size())
+    print(labels.size())
+
+    exit()
+
     predictions = np.argmax(logits, axis=-1)
 
-    precision = metric1.compute(
-        predictions=predictions, references=labels, average="micro"
-    )["precision"]
-    recall = metric2.compute(
-        predictions=predictions, references=labels, average="micro"
-    )["recall"]
-    f1 = metric3.compute(predictions=predictions, references=labels, average="micro")[
-        "f1"
-    ]
+    # precision = metric1.compute(
+    #     predictions=predictions, references=labels, average="micro"
+    # )["precision"]
+    # recall = metric2.compute(
+    #     predictions=predictions, references=labels, average="micro"
+    # )["recall"]
+    # f1 = metric3.compute(predictions=predictions, references=labels, average="micro")[
+    #     "f1"
+    # ]
     accuracy = metric4.compute(predictions=predictions, references=labels)["accuracy"]
 
-    return {"precision": precision, "recall": recall, "f1": f1, "accuracy": accuracy}
+    # return {"precision": precision, "recall": recall, "f1": f1, "accuracy": accuracy}
+    return {"accuracy": accuracy}
 
 
 def main(ai_name):
