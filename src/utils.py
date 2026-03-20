872f363fd457e16022171b5241dbbc619316bdd9
Anemily Machina <anemily.machina@gmail.com>
2026-03-20 14:41:44 -0400

    custom trainer that can display accuracy

--- src/utils.py
+++ src/utils.py
@@ -286,6 +286,9 @@ class CustomTrainer(Trainer):
 
                 self.confusion[l][p] += 1
 
+            print()
+            print(self.train_log_count)
+            print()
             self.train_log_count -= self._train_batch_size
 
             if self.train_log_count <= 0:
