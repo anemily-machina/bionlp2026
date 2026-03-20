7cf0f6e4991438659863ad840bc2a598a6223ea6
Anemily Machina <anemily.machina@gmail.com>
2026-03-20 14:27:50 -0400

    custom trainer that can display accuracy

--- src/utils.py
+++ src/utils.py
@@ -176,9 +176,9 @@ class CustomTrainer(Trainer):
                     correct[i] = v
 
         a = sum(correct) / sum(total_l)
-        class_r = [correct[i] / sum(total_l[i]) for i in range(num_c)]
+        class_r = [correct[i] / total_l[i] for i in range(num_c)]
         r = sum(class_r) / num_c
-        class_p = [correct[i] / sum(total_p[i]) for i in range(num_c)]
+        class_p = [correct[i] / total_p[i] for i in range(num_c)]
         p = sum(class_p) / num_c
 
         class_f1 = [
