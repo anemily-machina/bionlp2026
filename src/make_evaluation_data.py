ce7ee1a78734b0964031c34a9d3a2a12be43aa3c
Anemily Machina <anemily.machina@gmail.com>
2026-03-25 18:07:17 -0400

    script to take AI predictions to span predictions

--- src/make_evaluation_data.py
+++ src/make_evaluation_data.py
@@ -69,11 +69,10 @@ def make_annotaions(split, ai_model, eval_folder, tokenization_folder):
         attention_mask = attention_mask.unsqueeze(-1)
         attention_mask = attention_mask.to(device)
 
-        print(input_ids.size())
-        print(attention_mask.size())
-
         outputs = ai_model(input_ids=input_ids, attention_mask=attention_mask)
         logits = outputs.logits
+
+        logits = logits.squeeze()
 
         print(logits.size())
 
