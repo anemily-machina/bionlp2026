5d1ea1de870cb5d0e60a5279c766f70bdb709049
Anemily Machina <anemily.machina@gmail.com>
2026-03-26 09:57:40 -0400

    new training run

--- src/train_single_class.py
+++ src/train_single_class.py
@@ -112,8 +112,8 @@ def main(ai_name):
     training_args = TrainingArguments(
         output_dir="./data/checkpoints/single_class6",
         learning_rate=2e-4,
-        per_device_train_batch_size=2,
-        per_device_eval_batch_size=2,
+        per_device_train_batch_size=1,
+        per_device_eval_batch_size=1,
         num_train_epochs=100,
         weight_decay=0.02,
         warmup_steps=110,
@@ -124,7 +124,7 @@ def main(ai_name):
         save_strategy="epoch",
         load_best_model_at_end=True,
         seed=54321,
-        gradient_accumulation_steps=8,
+        gradient_accumulation_steps=16,
         metric_for_best_model="eval_f1",
         greater_is_better=True,
     )
